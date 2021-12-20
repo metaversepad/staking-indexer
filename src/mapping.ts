6ee8bc3a3997a4ea77de63cc2fd82fed1c3eb7b8
erwinphanglius <erwinphanglius10@gmail.com>
2021-12-21 02:12:19 +0700

    graph init

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,50 +1,55 @@
-import { BigInt } from "@graphprotocol/graph-ts"
-import { Staking, Staked, Unstaked } from "../generated/Staking/Staking"
-import { ExampleEntity } from "../generated/schema"
+import { Address, BigInt } from "@graphprotocol/graph-ts"
+import { Staked, Unstaked  } from "../generated/Staking/Staking"
+import { User, StakedEntity, UnstakedEntity } from "../generated/schema"
+
+export function addTotalDepositedByUser(address: Address, newValue: BigInt): void {
+  let id = address.toHexString();
+  let user = User.load(id);
+  if (user == null) {
+    user = new User(id);
+    user.address = address;
+    user.totalValue = BigInt.fromI32(0);
+  }
+  user.totalValue = user.totalValue.plus(newValue);
+  user.save();
+}
+
+export function substractTotalDepositedByUser(address: Address, newValue: BigInt): void {
+  let id = address.toHexString();
+  let user = User.load(id);
+  if (user == null) {
+    user = new User(id);
+    user.address = address;
+    user.totalValue = BigInt.fromI32(0);
+  }
+  user.totalValue = user.totalValue.minus(newValue);
+  user.save();
+}
 
 export function handleStaked(event: Staked): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
+  let entity = StakedEntity.load(event.transaction.hash.toHex())
+  
   if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+    entity = new StakedEntity(event.transaction.hash.toHex())
   }
+  addTotalDepositedByUser(event.params.from, event.params.amount);
+  
+  entity.from = event.params.from
+  entity.amount = event.params.amount
+  entity.save()
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+}
 
-  // Entity fields can be set based on event parameters
+export function handleUnstaked(event: Unstaked): void {
+  let entity = UnstakedEntity.load(event.transaction.from.toHex())
+
+  if (!entity) {
+    entity = new UnstakedEntity(event.transaction.from.toHex())
+  }
+  substractTotalDepositedByUser(event.params.from, event.params.amount);
+
   entity.from = event.params.from
   entity.amount = event.params.amount
 
-  // Entities can be written to the store with `.save()`
   entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.balanceOfStake(...)
-  // - contract.checkWithdrawInfo(...)
-  // - contract.tokenAddress(...)
-  // - contract.totalStaked(...)
 }
-
-export function handleUnstaked(event: Unstaked): void {}
