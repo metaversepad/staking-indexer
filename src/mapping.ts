cd3ef4bc7c0e670c4e9890eeadb621d75057536c
erwinphanglius <erwinphanglius10@gmail.com>
2022-01-03 13:39:32 +0700

    fix: change id unstaked to tx hash

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,5 +1,5 @@
 import { Address, BigInt } from "@graphprotocol/graph-ts"
-import { Staked, Unstaked  } from "../generated/Staking/Staking"
+import { Staked, Unstaked } from "../generated/Staking/Staking"
 import { User, StakedEntity, UnstakedEntity } from "../generated/schema"
 
 export function addTotalDepositedByUser(address: Address, newValue: BigInt): void {
@@ -28,12 +28,12 @@ export function substractTotalDepositedByUser(address: Address, newValue: BigInt
 
 export function handleStaked(event: Staked): void {
   let entity = StakedEntity.load(event.transaction.hash.toHex())
-  
+
   if (!entity) {
     entity = new StakedEntity(event.transaction.hash.toHex())
   }
   addTotalDepositedByUser(event.params.from, event.params.amount);
-  
+
   entity.from = event.params.from
   entity.amount = event.params.amount
   entity.save()
@@ -41,7 +41,7 @@ export function handleStaked(event: Staked): void {
 }
 
 export function handleUnstaked(event: Unstaked): void {
-  let entity = UnstakedEntity.load(event.transaction.from.toHex())
+  let entity = UnstakedEntity.load(event.transaction.hash.toHex())
 
   if (!entity) {
     entity = new UnstakedEntity(event.transaction.from.toHex())
