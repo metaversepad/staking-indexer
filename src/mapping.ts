7beb6643d9e5903411fbe7104aef3d11f65aaeb8
erwinphanglius <erwinphanglius10@gmail.com>
2022-01-03 13:53:58 +0700

    fix: add mapping for total staked

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,6 +1,6 @@
 import { Address, BigInt } from "@graphprotocol/graph-ts"
 import { Staked, Unstaked } from "../generated/Staking/Staking"
-import { User, StakedEntity, UnstakedEntity } from "../generated/schema"
+import { User, StakedEntity, UnstakedEntity, TotalStaked } from "../generated/schema"
 
 export function addTotalDepositedByUser(address: Address, newValue: BigInt): void {
   let id = address.toHexString();
@@ -28,23 +28,36 @@ export function substractTotalDepositedByUser(address: Address, newValue: BigInt
 
 export function handleStaked(event: Staked): void {
   let entity = StakedEntity.load(event.transaction.hash.toHex())
+  let totalStakedEntity = TotalStaked.load(event.address.toHex())
 
   if (!entity) {
     entity = new StakedEntity(event.transaction.hash.toHex())
   }
+
+  if (!totalStakedEntity) {
+    totalStakedEntity = new TotalStaked(event.address.toHex())
+  }
+
   addTotalDepositedByUser(event.params.from, event.params.amount);
 
   entity.from = event.params.from
   entity.amount = event.params.amount
   entity.save()
 
+  totalStakedEntity.amount = totalStakedEntity.amount.plus(event.params.amount);
+  totalStakedEntity.save()
 }
 
 export function handleUnstaked(event: Unstaked): void {
   let entity = UnstakedEntity.load(event.transaction.hash.toHex())
+  let totalStakedEntity = TotalStaked.load(event.address.toHex())
 
   if (!entity) {
     entity = new UnstakedEntity(event.transaction.hash.toHex())
+  }
+
+  if (!totalStakedEntity) {
+    totalStakedEntity = new TotalStaked(event.address.toHex())
   }
   substractTotalDepositedByUser(event.params.from, event.params.amount);
 
@@ -52,4 +65,7 @@ export function handleUnstaked(event: Unstaked): void {
   entity.amount = event.params.amount
 
   entity.save()
+
+  totalStakedEntity.amount = totalStakedEntity.amount.minus(event.params.amount);
+  totalStakedEntity.save()
 }
